eeea58e8e37a2e2402bbd551426f9f18e9f0f8fb
aleex1848 <halycon01@gmail.com>
2025-09-09 16:05:48 +0200

    fix

--- src/models/BenutzerAbwesenheit.php
+++ src/models/BenutzerAbwesenheit.php
@@ -8,14 +8,14 @@ class BenutzerAbwesenheit
 {
     public ?string $userid;
     public bool $abwesend;
-    public ?User $user;
-    public ?User $vertreter;
+    public ?Illuminate\Contracts\Auth\Authenticatable $user;
+    public ?Illuminate\Contracts\Auth\Authenticatable $vertreter;
     
     public function __construct($data)
     {
         $this->userid = $data['userId'];
         $this->abwesend = $data['isAbsent'];
-        $this->user = app(config('d3-rest-laravel.USER_MODEL')::class)::firstWhere('username', d3RestLaravelFacade::getUsernameByUserId($data['userId']));
+        $this->user = app(config('d3-rest-laravel.USER_MODEL'))::firstWhere('username', d3RestLaravelFacade::getUsernameByUserId($data['userId']));
         $this->vertreter = ($this->abwesend && $data['nextPresentDeputyId']) ? app(config('d3-rest-laravel.USER_MODEL'))::firstWhere('username', d3RestLaravelFacade::getUsernameByUserId($data['nextPresentDeputyId'])) : null;
     }
 }
